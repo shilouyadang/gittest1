d3adbfa95206db14a00240e1b7a31ddfcd46f8b7
diandian <2383918707@qq.com>
2023-09-04 14:30:02 +0800

    3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("holle,git2");
+        System.out.println("holle,git3");
     }
 }
